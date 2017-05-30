95e7ee66dd91433934c072b50b49da37299307ee
Rodrigo Baldini <rbaldini69@hotmail.com>
2017-05-29 22:47:23 -0300

    Inserto Backgrounworker

--- ganadores/FrmPrincipal.Designer.vb
+++ ganadores/FrmPrincipal.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,24 +20,37 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.txtArchivo = New System.Windows.Forms.Button()
         Me.gbxExtractos = New System.Windows.Forms.GroupBox()
-        Me.txtValorApuesta = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtCantidadApuestas = New System.Windows.Forms.TextBox()
+        Me.txtRecaudado = New System.Windows.Forms.TextBox()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.gbxExtractos.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(27, 95)
+        Me.Button1.Location = New System.Drawing.Point(12, 95)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(90, 51)
+        Me.Button1.Size = New System.Drawing.Size(105, 51)
         Me.Button1.TabIndex = 31
         Me.Button1.Text = "Limpiar"
         Me.Button1.UseVisualStyleBackColor = True
@@ -45,16 +58,27 @@ Partial Class Form1
         'txtArchivo
         '
         Me.txtArchivo.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtArchivo.Location = New System.Drawing.Point(27, 26)
+        Me.txtArchivo.Location = New System.Drawing.Point(12, 26)
         Me.txtArchivo.Name = "txtArchivo"
-        Me.txtArchivo.Size = New System.Drawing.Size(90, 51)
+        Me.txtArchivo.Size = New System.Drawing.Size(105, 51)
         Me.txtArchivo.TabIndex = 23
         Me.txtArchivo.Text = "Archivo"
         Me.txtArchivo.UseVisualStyleBackColor = True
         '
         'gbxExtractos
         '
-        Me.gbxExtractos.Controls.Add(Me.txtValorApuesta)
+        Me.gbxExtractos.Controls.Add(Me.Label6)
+        Me.gbxExtractos.Controls.Add(Me.Label5)
+        Me.gbxExtractos.Controls.Add(Me.Label4)
+        Me.gbxExtractos.Controls.Add(Me.Label3)
+        Me.gbxExtractos.Controls.Add(Me.Label2)
+        Me.gbxExtractos.Controls.Add(Me.Label1)
+        Me.gbxExtractos.Controls.Add(Me.TextBox5)
+        Me.gbxExtractos.Controls.Add(Me.TextBox4)
+        Me.gbxExtractos.Controls.Add(Me.TextBox3)
+        Me.gbxExtractos.Controls.Add(Me.TextBox2)
+        Me.gbxExtractos.Controls.Add(Me.txtCantidadApuestas)
+        Me.gbxExtractos.Controls.Add(Me.txtRecaudado)
         Me.gbxExtractos.Location = New System.Drawing.Point(123, 26)
         Me.gbxExtractos.Name = "gbxExtractos"
         Me.gbxExtractos.Size = New System.Drawing.Size(603, 289)
@@ -62,21 +86,131 @@ Partial Class Form1
         Me.gbxExtractos.TabStop = False
         Me.gbxExtractos.Text = "Extracto Archivo"
         '
-        'txtValorApuesta
+        'Label6
         '
-        Me.txtValorApuesta.Location = New System.Drawing.Point(147, 31)
-        Me.txtValorApuesta.Name = "txtValorApuesta"
-        Me.txtValorApuesta.Size = New System.Drawing.Size(100, 20)
-        Me.txtValorApuesta.TabIndex = 33
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(28, 227)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 44
+        Me.Label6.Text = "Label6"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 190)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 43
+        Me.Label5.Text = "Label5"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 42
+        Me.Label4.Text = "Label4"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 41
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 13)
+        Me.Label2.TabIndex = 40
+        Me.Label2.Text = "Cantidad de Apuestas"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 39
+        Me.Label1.Text = "Total Recaudado"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(148, 220)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 38
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(148, 183)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 37
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(148, 145)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 36
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(148, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 35
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtCantidadApuestas
+        '
+        Me.txtCantidadApuestas.Location = New System.Drawing.Point(147, 69)
+        Me.txtCantidadApuestas.Name = "txtCantidadApuestas"
+        Me.txtCantidadApuestas.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidadApuestas.TabIndex = 34
+        Me.txtCantidadApuestas.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtRecaudado
+        '
+        Me.txtRecaudado.Location = New System.Drawing.Point(147, 31)
+        Me.txtRecaudado.Name = "txtRecaudado"
+        Me.txtRecaudado.Size = New System.Drawing.Size(100, 20)
+        Me.txtRecaudado.TabIndex = 33
+        Me.txtRecaudado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'ProgressBar1
         '
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.Coral
         Me.ProgressBar1.Location = New System.Drawing.Point(123, 339)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(602, 28)
+        Me.ProgressBar1.Step = 1
         Me.ProgressBar1.TabIndex = 33
         '
         'Timer1
+        '
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.btnCancelar.Location = New System.Drawing.Point(12, 167)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(105, 51)
+        Me.btnCancelar.TabIndex = 35
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
         '
         '
         'Form1
@@ -84,6 +218,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(862, 528)
+        Me.Controls.Add(Me.btnCancelar)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.gbxExtractos)
         Me.Controls.Add(Me.Button1)
@@ -101,7 +236,20 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents txtArchivo As Button
     Friend WithEvents gbxExtractos As GroupBox
-    Friend WithEvents txtValorApuesta As TextBox
-    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents txtRecaudado As TextBox
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtCantidadApuestas As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
 End Class
--- ganadores/FrmPrincipal.vb
+++ ganadores/FrmPrincipal.vb
@@ -3,8 +3,10 @@ Imports System
 Imports System.IO
 Imports System.Windows.Forms
 Imports System.Text
+Imports System.ComponentModel
 
 Public Class Form1
+    Dim ruta As String
     Dim cantRegistros As Integer = 0
     Dim cantRegistros2 As Integer = 0
     'posicion
@@ -30,16 +32,11 @@ Public Class Form1
     Dim valorTotalApuestas As Long = 0
 
 
-    Dim ruta As String
     'Dim frmresul As New frmResultados()
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'frmresul.Show()
-        'frmresul.Location = New Point(1500, 600)
-        'frmresul.WindowState = FormWindowState.Maximized
-        'Me.txtUnidad.Focus()
+
     End Sub
     Private Sub Label2_Click(sender As Object, e As EventArgs)
-
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs)
@@ -117,7 +114,7 @@ Public Class Form1
     End Sub
 
     Private Sub txtArchivo_Click(sender As Object, e As EventArgs) Handles txtArchivo.Click
-        Dim ruta As String
+
         Dim Openf As New OpenFileDialog
         Dim registros As New ArrayList()
         'Dim vendido As String
@@ -133,8 +130,7 @@ Public Class Form1
                 End If
             Next
             Dim lector As StreamReader = New StreamReader(ruta)
-            Dim lector2 As StreamReader = New StreamReader(ruta)
-            Dim linea As String
+
             Dim linea0 As String
             Do
                 linea0 = lector.ReadLine()
@@ -142,27 +138,14 @@ Public Class Form1
 
 
             Loop Until linea0 Is Nothing
-            ProgressBar1.Maximum = cantRegistros
-            '        Timer1.Enabled = True
-
+            ProgressBar1.Value = 0
+            '    
             MsgBox("fin de lectura de archivo, cant. de registros: " & cantRegistros)
             lector.Close()
+            BackgroundWorker1.WorkerReportsProgress = True
+            BackgroundWorker1.WorkerSupportsCancellation = True
 
-            Do
-                linea = lector2.ReadLine()
-                Me.valorDeApuesta = Mid(linea, 71, 8)
-                cantRegistros2 += 1
-                Dim auxvalor As Long
-                Long.TryParse(valorDeApuesta, auxValor)
-                valorTotalApuestas = valorTotalApuestas + auxvalor
-
-                ProgressBar1.Value += 1
-            Loop Until linea Is Nothing
-            Me.txtValorApuesta.Text = Format(CDec(valorTotalApuestas), "C")
-
-
-            lector2.Close()
-
+            BackgroundWorker1.RunWorkerAsync()
 
         End If
 
@@ -172,13 +155,7 @@ Public Class Form1
 
     End Sub
     Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
-        For Each Ctrl As Control In Me.gbxExtractos.Controls
-            If TypeOf (Ctrl) Is TextBox Then
-                CType(Ctrl, TextBox).Clear()
-                CType(Ctrl, TextBox).ForeColor = Color.Black
-                CType(Ctrl, TextBox).BackColor = Color.White
-            End If
-        Next
+
         For Each Ctrl As Control In Me.gbxExtractos.Controls
             If TypeOf (Ctrl) Is TextBox Then
                 CType(Ctrl, TextBox).Clear()
@@ -254,6 +231,65 @@ Public Class Form1
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        ProgressBar1.Value = ProgressBar1.Value * 100 / cantRegistros
+        'ProgressBar1.Value = ProgressBar1.Value * 100 / cantRegistros
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim lector2 As StreamReader = New StreamReader(ruta)
+        Dim linea As String
+        Do
+            linea = lector2.ReadLine()
+            Me.valorDeApuesta = Mid(linea, 71, 8)
+            cantRegistros2 += 1
+            Dim auxvalor As Long
+            Long.TryParse(valorDeApuesta, auxvalor)
+            valorTotalApuestas = valorTotalApuestas + auxvalor
+
+
+
+            REM HAY PENDIENTE UNA CANCELACION???
+            If BackgroundWorker1.CancellationPending Then
+                e.Cancel = True
+                Exit Sub
+            End If
+            BackgroundWorker1.ReportProgress(CType((100 * cantRegistros2 / cantRegistros), Long))
+        Loop Until linea Is Nothing
+        'ProgressBar1.Maximum = cantRegistros2
+
+        lector2.Close()
+
+
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        BackgroundWorker1.CancelAsync()
+
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If Not e.Error Is Nothing Then
+
+            REM MUESTRO EL ERROR
+            MessageBox.Show(e.Error.Message)
+
+        ElseIf e.Cancelled Then
+
+            REM SE CANCELO LA OPERACION??
+            MessageBox.Show("Operación cancelada", "BackGroundWorker", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+        Else
+
+            REM LA OPERACION FINALIZO CORRECTAMENTE
+            Me.txtRecaudado.Text = Format(CDec(valorTotalApuestas), "C")
+            Me.txtCantidadApuestas.Text = cantRegistros2
+
+            MessageBox.Show("Tarea finalizada!!!", "BackGroundWorker", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+
+        End If
+
     End Sub
 End Class
